fa0bbec5aadb31d981308e17ae5a208b713dbca3
joe.c.starr <joe.c.starr@gmail.com>
2023-06-02 00:02:53 -0500

    add template module

--- misc/template_module/template.c
+++ misc/template_module/template.c
@@ -1,19 +1,18 @@
 /*!
- *  @file generator_pattern.c
+ *  @file template.c
  *
- *  @brief  A pattern for generator modules
+ *  @brief
  *
  *
- *  @author    Isabel Darcy
- *  @author    Ethan Rooke
- *  @author    Zachary Bryhtan
- *  @author    Joe Starr
+ *  @author
  *
  */
 
-#include "generator_rational.h"
-#include <stddef.h>
-gen_rational_config_t *config;
+#include "template.h"
+
+/******************************************************************************/
+/************************** Includes ******************************************/
+/******************************************************************************/
 
 /******************************************************************************/
 /************************** Defines *******************************************/
@@ -31,16 +30,16 @@ gen_rational_config_t *config;
 /************************** Public Function Definitions ***********************/
 /******************************************************************************/
 
-/*
- *  Documentation in header
+/*!
+ * @brief
+ *
+ * @return int
  */
-int gen_rational_config(gen_rational_config_t *config_arg)
+int template()
 {
-    int ret_val = GEN_RATIONAL_CONFIG_FAIL;
-    if (config != NULL)
-    {
-        config = config_arg;
-        ret_val = GEN_RATIONAL_CONFIG_SUCCESS;
-    }
-    return ret_val;
+    return 0; /****************************************************************/
 }
+
+/******************************************************************************/
+/************************** Private Function Declerations *********************/
+/******************************************************************************/
