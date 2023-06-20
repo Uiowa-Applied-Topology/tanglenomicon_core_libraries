1b38806efadfa624eb93d0b7feaa4113bd56591a
joe.c.starr <joe.c.starr@gmail.com>
2023-06-20 00:25:24 -0500

    add special case handling.

--- source/generators/rational/generator_rational.c
+++ source/generators/rational/generator_rational.c
@@ -23,14 +23,15 @@
 /******************************************************************************/
 
 /******************************************************************************/
-/************************** Local Variables ***********************************/
+/************************** Typedefs ******************************************/
 /******************************************************************************/
 
 /*!
  * @brief
  *
  */
-static gen_rational_config_t *config = NULL;
+typedef bool (*gen_rational_perm_t)(gen_rational_config_t*);
+
 
 /******************************************************************************/
 /************************** Private Function Declarations *********************/
@@ -39,32 +40,32 @@ static gen_rational_config_t *config = NULL;
 /*!
  * @brief
  *
+ * @param cfg
  * @return uint8_t
  */
-static uint8_t gen_rational_accel_asc();
+static uint8_t gen_rational_accel_asc(gen_rational_config_t* cfg);
 
 /*!
  * @brief
  *
- *
+ * @param cfg
  * @return uint8_t
  */
-static uint8_t gen_rational_permute();
+static uint8_t gen_rational_permute(gen_rational_config_t* cfg);
 
 /*!
  * @brief
  *
- * @param A
- * @param length
+ * @param cfg
  * @return uint8_t
  */
-static uint8_t gen_rational_heaps();
+static uint8_t gen_rational_heaps(gen_rational_config_t* cfg);
 
 /*!
  * @brief
  *
- * @param i
- * @param j
+ * @param left_p
+ * @param right_p
  * @return uint8_t
  */
 static uint8_t gen_rational_swap_in_tv(uint8_t *left_p, uint8_t *right_p);
@@ -72,21 +73,62 @@ static uint8_t gen_rational_swap_in_tv(uint8_t *left_p, uint8_t *right_p);
 /*!
  * @brief
  *
+ * @param cfg
  * @return uint8_t
  */
-static uint8_t gen_rational_tvrev();
+static uint8_t gen_rational_tvrev(gen_rational_config_t* cfg);
 
 /*!
  * @brief
  *
- *
- * @return
+ * @param cfg
+ * @return uint8_t
  */
-static uint8_t gen_rational_write();
+static uint8_t gen_rational_write(gen_rational_config_t* cfg);
+
+/*!
+ * @brief
+ *
+ * @param cfg
+ * @return true
+ * @return false
+ */
+static bool gen_rational_perm_all_matching(gen_rational_config_t* cfg);
+
+/*!
+ * @brief
+ *
+ * @param cfg
+ * @return true
+ * @return false
+ */
+static bool gen_rational_perm_default(gen_rational_config_t* cfg);
 
 /******************************************************************************/
-/************************** Public Function Definitions
- ***********************/
+/************************** Local Variables ***********************************/
+/******************************************************************************/
+
+/*!
+ * @brief
+ *
+ */
+static gen_rational_config_t *gen_rational_localcfg = NULL;
+
+/*!
+ * @brief
+ *
+ */
+static gen_rational_perm_t gen_rational_perm_funs[] = {
+    &gen_rational_perm_all_matching, &gen_rational_perm_default};
+
+/*!
+ * @brief
+ *
+ */
+static size_t gen_rational_perm_funs_len = 2u;
+
+/******************************************************************************/
+/************************** Public Function Definitions ***********************/
 /******************************************************************************/
 
 /*
@@ -110,12 +152,13 @@ uint8_t gen_rational_config(gen_rational_config_t *config_arg)
     }
     else
     {
-        config = config_arg;
+        gen_rational_localcfg = config_arg;
 
         ret_val = GEN_DEFS_CONFIG_SUCCESS;
     }
     return ret_val;
 }
+
 /*
  *  Documentation in header
  */
@@ -123,16 +166,17 @@ uint8_t gen_rational_generate()
 {
 
     uint8_t ret_val = GEN_DEFS_GENERATION_FAIL;
-    if (config == NULL)
+    if (gen_rational_localcfg == NULL)
     {
         ret_val |= GEN_RATIONAL_CONFIG_IS_NULL;
     }
     else
     {
-        (void)gen_rational_accel_asc();
+        (void)gen_rational_accel_asc(gen_rational_localcfg);
     }
     return ret_val;
 }
+
 /******************************************************************************/
 /************************** Private Function Declarations *********************/
 /******************************************************************************/
@@ -140,14 +184,14 @@ uint8_t gen_rational_generate()
 /*
  *  Documentation at Declaration
  */
-uint8_t gen_rational_accel_asc()
+uint8_t gen_rational_accel_asc(gen_rational_config_t* cfg)
 {
     uint8_t ret_val = GEN_DEFS_GENERATION_SUCCESS;
-    uint8_t *a = config->tv_n->twist_vector;
-    size_t *len = &(config->tv_n->tv_length);
+    uint8_t *a = cfg->tv_n->twist_vector;
+    size_t *len = &(cfg->tv_n->tv_length);
 
     uint8_t l = 0;
-    uint8_t n = config->crossingNumber;
+    uint8_t n = cfg->crossingNumber;
     uint8_t k = 1;
     uint8_t y = n - 1;
     uint8_t x = 0;
@@ -170,7 +214,7 @@ uint8_t gen_rational_accel_asc()
             if (k % 2 == 1)
             {
                 *len = k + 2;
-                gen_rational_permute();
+                gen_rational_permute(cfg);
             }
             x += 1;
             y -= 1;
@@ -180,25 +224,28 @@ uint8_t gen_rational_accel_asc()
         if (k % 2 == 1)
         {
             *len = k + 2;
-            gen_rational_permute();
+            gen_rational_permute(cfg);
         }
     }
     *len = 1;
-    a[0] = config->crossingNumber;
-    (void)gen_rational_write();
+    a[0] = cfg->crossingNumber;
+    (void)gen_rational_write(cfg);
     return ret_val;
 }
 
 /*
  *  Documentation at Declaration
  */
-uint8_t gen_rational_permute()
+uint8_t gen_rational_permute(gen_rational_config_t* cfg)
 {
     uint8_t ret_val = GEN_DEFS_GENERATION_SUCCESS;
 
     //@@@TODO: we need to add heuristics here to cut down on compares.
-    (void)gen_rational_heaps();
-    (void)gen_rational_tvrev();
+    size_t i = 0;
+    while (((gen_rational_perm_funs[i])(cfg)) == false)
+    {
+        i++;
+    }
 
     return ret_val;
 }
@@ -206,14 +253,14 @@ uint8_t gen_rational_permute()
 /*
  *  Documentation at Declaration
  */
-uint8_t gen_rational_heaps()
+uint8_t gen_rational_heaps(gen_rational_config_t* cfg)
 {
     uint8_t ret_val = GEN_DEFS_GENERATION_SUCCESS;
 
     uint8_t c[UTIL_TANG_DEFS_MAX_CROSSINGNUM] = {0};
 
-    uint8_t *a = config->tv_n->twist_vector;
-    size_t length = config->tv_n->tv_length;
+    uint8_t *a = cfg->tv_n->twist_vector;
+    size_t length = cfg->tv_n->tv_length;
 
     size_t i = 0;
 
@@ -221,7 +268,7 @@ uint8_t gen_rational_heaps()
     {
         if (i == 0)
         {
-            (void)gen_rational_write();
+            (void)gen_rational_write(cfg);
         }
         if (c[i] < i)
         {
@@ -262,23 +309,23 @@ uint8_t gen_rational_swap_in_tv(uint8_t *left_p, uint8_t *right_p)
 /*
  *  Documentation at declaration
  */
-uint8_t gen_rational_write()
+uint8_t gen_rational_write(gen_rational_config_t* cfg)
 {
     uint8_t ret_val = GEN_DEFS_GENERATION_SUCCESS;
-    note_tv_decode(*(config->tv_n), config->tv_str_buff);
-    config->storage_write("key", config->tv_str_buff, config->tv_str_buff);
+    note_tv_decode(*(cfg->tv_n), cfg->tv_str_buff);
+    cfg->storage_write("key", cfg->tv_str_buff, cfg->tv_str_buff);
     return ret_val;
 }
 
 /*
  *  Documentation at declaration
  */
-uint8_t gen_rational_tvrev()
+uint8_t gen_rational_tvrev(gen_rational_config_t* cfg)
 {
     uint8_t ret_val = GEN_DEFS_GENERATION_SUCCESS;
 
-    uint8_t *tv = config->tv_n->twist_vector;
-    uint8_t length = config->tv_n->tv_length;
+    uint8_t *tv = cfg->tv_n->twist_vector;
+    uint8_t length = cfg->tv_n->tv_length;
 
     uint8_t *left_p, *right_p;
 
@@ -297,4 +344,38 @@ uint8_t gen_rational_tvrev()
     }
 
     return ret_val;
+}
+
+
+bool gen_rational_perm_all_matching(gen_rational_config_t* cfg)
+{
+    bool ret_val = true;
+
+    uint8_t *tv = cfg->tv_n->twist_vector;
+    uint8_t length = cfg->tv_n->tv_length;
+
+    uint8_t i = 0;
+    for (i = length-1;i>0;i--)
+    {
+        if(tv[i-1]!=tv[i])
+        {
+            ret_val = false;
+            break;
+        }
+    }
+
+    if(ret_val==true)
+    {
+        (void)gen_rational_write(cfg);
+    }
+
+    return ret_val;
+}
+
+
+bool gen_rational_perm_default(gen_rational_config_t* cfg)
+{
+    (void)gen_rational_heaps(cfg);
+    (void)gen_rational_tvrev(cfg);
+    return true;
 }
