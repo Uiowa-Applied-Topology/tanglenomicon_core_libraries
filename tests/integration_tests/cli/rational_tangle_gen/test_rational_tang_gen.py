c0b23ab0b8a6eff9650564f3ca0b3384fbec12a4
joe.c.starr <joe.c.starr@gmail.com>
2023-07-09 15:57:10 -0500

    Update test to run in ctest in gh action.

--- tests/integration_tests/cli/rational_tangle_gen/test_rational_tang_gen.py
+++ tests/integration_tests/cli/rational_tangle_gen/test_rational_tang_gen.py
@@ -17,9 +17,9 @@ else:
 def test_tangles():
 
     out_file = Path.cwd() / "res.json"
-    arg_list = f"{exepath} -r -n 5 -j -f\"{out_file}\""
+    arg_list = f" -r -n 5 -j -f\"{out_file}\""
 
-    with subprocess.Popen(arg_list, stdout=subprocess.PIPE) as proc:
+    with subprocess.Popen([exepath,arg_list], stdout=subprocess.PIPE) as proc:
         proc.wait()
         assert proc.returncode == 0
         # @@@ TODO: add checks for correctness
